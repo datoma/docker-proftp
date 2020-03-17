72c4ffe92c8b99ef10e50b5dceb0bff63f05e663
Michael Hoffmann <datoma@users.noreply.github.com>
2020-03-17 08:10:07 +0100

    init

---
+++ docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Attention: dont use ":" or ";" in password or username
+
+if [ -n "$FTP_LIST" ]; then
+	IFS=';' read -r -a parsed_ftp_list <<< "$FTP_LIST" ; unset IFS
+	for ftp_account in ${parsed_ftp_list[@]}
+	do
+		IFS=':' read -r -a tab <<< "$ftp_account" ; unset IFS
+		ftp_login=${tab[0]}
+		ftp_pass=${tab[1]}
+		CRYPTED_PASSWORD=$(perl -e 'print crypt($ARGV[0], "password")' $ftp_pass)
+		mkdir /home/$ftp_login
+		useradd --shell /bin/sh ${USERADD_OPTIONS} -d /home/$ftp_login --password $CRYPTED_PASSWORD $ftp_login
+		chown -R $ftp_login:$ftp_login /home/$ftp_login
+	done
+fi
+
+exec "$@"
