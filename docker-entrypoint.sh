cc3382e45f81b30126c2a322ea081b00398f1371
Michael Hoffmann <github@datoma.de>
2020-03-18 17:54:44 +0100

    changed passwd behaviour

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,10 +8,11 @@ if [ -n "$FTP_LIST" ]; then
 		IFS=':' read -r -a tab <<< "$ftp_account" ; unset IFS
 		ftp_login=${tab[0]}
 		ftp_pass=${tab[1]}
-		CRYPTED_PASSWORD=$(perl -e 'print crypt($ARGV[0], "password")' $ftp_pass)
+#		CRYPTED_PASSWORD=$(perl -e 'print crypt($ARGV[0], "password")' $ftp_pass)
 		mkdir /home/$ftp_login
                 addgroup -S $ftp_login
-                echo "$CRYPTED_PASSWORD\n$CRYPTED_PASSWORD" | adduser -S $ftp_login -G $ftp_login -h /home/$ftp_login
+#                echo "$CRYPTED_PASSWORD\n$CRYPTED_PASSWORD" | adduser -S $ftp_login -G $ftp_login -h /home/$ftp_login
+                echo "$ftp_pass\n$ftp_pass" | adduser -S $ftp_login -G $ftp_login -h /home/$ftp_login
 		chown -R $ftp_login:$ftp_login /home/$ftp_login
 	done
 fi
